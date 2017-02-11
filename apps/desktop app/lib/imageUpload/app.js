d6697496e46c824a62e005bb9a375650e91a3781
nguyenpham95 <phamkhoinguyen1995@gmail.com>
2017-02-11 22:11:47 +0700

    Create Desktop App

---
+++ apps/desktop app/lib/imageUpload/app.js
@@ -0,0 +1,35 @@
+
+
+var UploadController = function ($scope, fileReader) {
+     console.log(fileReader)
+    $scope.getFile = function () {
+        $scope.progress = 0;
+        fileReader.readAsDataUrl($scope.file, $scope)
+                      .then(function(result) {
+                          $scope.imageSrc = result;
+                      });
+    };
+ 
+    $scope.$on("fileProgress", function(e, progress) {
+        $scope.progress = progress.loaded / progress.total;
+    });
+ 
+};
+
+app.directive("ngFileSelect",function(){
+
+  return {
+    link: function($scope,el){
+      
+      el.bind("change", function(e){
+      
+        $scope.file = (e.srcElement || e.target).files[0];
+        $scope.getFile();
+      })
+      
+    }
+    
+  }
+  
+  
+})
