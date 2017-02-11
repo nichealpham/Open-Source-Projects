efcfdbd49d448d75b44f431a3e7491f4fcdc3c65
nguyenpham95 <phamkhoinguyen1995@gmail.com>
2017-02-11 22:47:50 +0700

    Update Desktop App

--- apps/desktop app/bom_desktop-win32-ia32/resources/app/main.js
+++ apps/desktop app/bom_desktop-win32-ia32/resources/app/main.js
@@ -13,7 +13,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1280, height: 760})
+  mainWindow = new BrowserWindow({width: 1320, height: 800})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
