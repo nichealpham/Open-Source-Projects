e1cd7474a7922b75825b4b98b3d1345cf169abfb
Phạm Khôi Nguyên <nguyenpham95@users.noreply.github.com>
2017-07-08 17:23:27 +0700

    Small update for Web-based version :dango:

--- js/controllers.js
+++ js/controllers.js
@@ -1496,13 +1496,13 @@
     };
     $window.localStorage["BOM_company"] = JSON.stringify($scope.company);
   };
-  const ipc = require('electron').ipcRenderer;
+  // const ipc = require('electron').ipcRenderer;
 
-  const printPDFBtn = document.getElementById('print-pdf');
+  // const printPDFBtn = document.getElementById('print-pdf');
 
-  printPDFBtn.addEventListener('click', function (event) {
-    ipc.send('print-to-pdf');
-  });
+  // printPDFBtn.addEventListener('click', function (event) {
+  //   ipc.send('print-to-pdf');
+  // });
 
   // ipc.on('wrote-pdf', function (event, path) {
   //   const message = `Wrote PDF to: ${path}`;
